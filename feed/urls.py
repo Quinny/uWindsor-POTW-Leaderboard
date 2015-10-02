7631804d794b1bcdd2189c7d56338846d8885dbf
Quinn Perfetto <thequinnftw@gmail.com>
2015-10-02 15:10:40 -0400

    added feed for leaderboard

--- feed/urls.py
+++ feed/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^.xml/?$', views.LatestProblem()),
+    url(r'^problem/?$', views.LatestProblem()),
+    url(r'^solvers/?$', views.LeaderBoard()),
 )
--- feed/views.py
+++ feed/views.py
@@ -1,10 +1,10 @@
 from django.contrib.syndication.views import Feed
 from problem.models import Problem
-import HTMLParser
+from student.models import Student
 
 class LatestProblem(Feed):
     title = "Latest Problem Of The Week"
-    link = "/feed.xml"
+    link = "/feed/problem"
 
     def items(self):
         latest = Problem.objects.filter(published=True).order_by("-week")[0]
@@ -19,3 +19,22 @@ class LatestProblem(Feed):
 
     def item_link(self, item):
         return "/problem/" + str(item.year) + "/" + str(item.week)
+
+class LeaderBoard(Feed):
+    title = "Top Problem Solvers"
+    link = "/feed/solvers"
+
+    def items(self):
+        students = Student.objects.all()
+        s = sorted(students, key=lambda x: x.solution_count, reverse=True)[:5]
+        return ["\n".join(map(lambda x: x.student_id, s))]
+
+    def item_title(self, item):
+        return "Top Problem Solvers"
+
+    def item_description(self, item):
+        return item
+
+    def item_link(self, item):
+        return "/solvers"
+
