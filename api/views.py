36c293cdfc44bb8b9f6fb7ae4806cf5d554e9440
Quinn Perfetto <thequinnftw@gmail.com>
2015-10-06 08:57:48 -0400

    added cors response

--- api/views.py
+++ api/views.py
@@ -1,10 +1,18 @@
 from django.http import JsonResponse
 from student.models import Student
 
+def cors_json(resp):
+    r = JsonResponse(resp)
+    r['Access-Control-Allow-Origin'] = '*'
+    r['Access-Control-Allow-Methods'] = "GET"
+    r['Access-Control-Allow-Headers'] = 'X-Requested-With,content-type'
+    return r
+
 def solvers(request):
     ret = []
     students = Student.objects.all()
 
     for s in students:
         ret.append({'student_id': s.student_id, 'solved': s.solution_count})
-    return JsonResponse({'data': ret})
+    return cors_json({'data' : ret})
+    #return JsonResponse({'data': ret})
