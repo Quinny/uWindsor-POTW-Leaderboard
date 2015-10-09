0e39c86fdde6e81c17444493fc6c0cb298fd83fd
Quinn Perfetto <thequinnftw@gmail.com>
2015-10-09 09:29:01 -0400

    added fake settings file

---
+++ potwleaderboard/fake-settings.py
@@ -0,0 +1,106 @@
+"""
+Django settings for potwleaderboard project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import socket
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 's3cretz'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+TEMPLATE_DEBUG = True
+ALLOWED_HOSTS = [".quinnftw.com"]
+
+# Application definition
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'student',
+    'solution',
+    'problem',
+    'errorpage',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'potwleaderboard.urls'
+
+WSGI_APPLICATION = 'potwleaderboard.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+        BASE_DIR + '/static/',
+)
+
+TEMPLATE_DIRS = (
+        BASE_DIR + '/templates/',
+)
+
+MEDIA_ROOT = BASE_DIR + '/uploads/'
+MEDIA_URL  = '/uploads/'
+
+
+
+EMAIL_USE_SSL = True
+EMAIL_HOST = ""
+EMAIL_PORT = 465
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+
+EMAIL_SECRET = "super-secret"
+
+if DEBUG:
+    SITE_URL = "http://localhost:8000"
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    SITE_URL = "http://potw.quinnftw.com"
