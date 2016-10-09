e270bb321625fb42fe8c199b8b4cd67434a4aa03
Quinn Perfetto <thequinnftw@gmail.com>
2016-10-09 09:42:03 -0400

    added 404 message

--- errorpage/views.py
+++ errorpage/views.py
@@ -7,6 +7,11 @@ def index(request):
 def random_quote():
     return random.choice([
         "Proof to me this page does not exist",
+
         "Well its really quite simple you know, this page does not exist",
+
         "Class, are you with me?  This page does not exist!",
+
+        "I have gained an immense amount of knowledge in my time, and I can safely "
+        + "conclude that this page in fact does not exist",
     ])
